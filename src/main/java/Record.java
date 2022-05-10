830b61e437d491ddcebf2db9032111a11d2058e8
kkter <01168965@pw.edu.pl>
2022-05-10 21:42:22 +0200

    Final version

--- src/main/java/Record.java
+++ src/main/java/Record.java
@@ -2,12 +2,12 @@
  * Objects of this class are records in data set
  */
 public class Record {
-    int ID;
-    private float SepalLengthCm;
-    private float SepalWidthCm;
-    private float PetalLengthCm;
-    private float PetalWidthCm;
-    private String species;
+    final private int ID;
+    final private float SepalLengthCm;
+    final private float SepalWidthCm;
+    final private float PetalLengthCm;
+    final private float PetalWidthCm;
+    final private String species;
 
     public Record(int ID,float SepalLengthCm, float SepalWidthCm,float PetalLengthCm, float PetalWidthCm, String species){
         this.ID=ID;
@@ -22,47 +22,25 @@ public class Record {
         return ID;
     }
 
-    public void setID(int ID) {
-        this.ID = ID;
-    }
-
     public float getSepalLengthCm() {
         return SepalLengthCm;
-    }
-
-    public void setSepalLengthCm(float sepalLengthCm) {
-        SepalLengthCm = sepalLengthCm;
     }
 
     public float getSepalWidthCm() {
         return SepalWidthCm;
     }
 
-    public void setSepalWidthCm(float sepalWidthCm) {
-        SepalWidthCm = sepalWidthCm;
-    }
-
     public float getPetalLengthCm() {
         return PetalLengthCm;
     }
 
-    public void setPetalLengthCm(float petalLengthCm) {
-        PetalLengthCm = petalLengthCm;
-    }
 
     public float getPetalWidthCm() {
         return PetalWidthCm;
     }
 
-    public void setPetalWidthCm(float petalWidthCm) {
-        PetalWidthCm = petalWidthCm;
-    }
 
     public String getSpecies() {
         return species;
-    }
-
-    public void setSpecies(String species) {
-        this.species = species;
     }
 }
